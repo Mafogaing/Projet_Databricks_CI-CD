ad7725b771cf0c221516fdd65450c13bd27805f8
Mafogaing <mafogaingrose@gmail.com>
2024-10-31 14:10:50 +0100

    Modification du code dans test_train_model.py

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -1,5 +1,10 @@
 from notebooks.train_model import main
 import os
+import sys
+from os.path import abspath, dirname
+
+sys.path.insert(0, abspath(dirname(dirname(__file__))))
+
 
 def test_model_training():
     # Run the main function
@@ -8,3 +13,5 @@ def test_model_training():
     assert os.path.exists("mlruns"), "MLflow run not found"
 
     # Additional tests can be added here
+
+#$env:PYTHONPATH = "E:\Projet_Databricks_CI-CD"; pytest tests/  (commande d'exécution)
